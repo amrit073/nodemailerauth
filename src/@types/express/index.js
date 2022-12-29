6d990b7f3c3c04346aefbe5c9df4669eac07585f
AmritPaudel <46588187+amrit073@users.noreply.github.com>
2022-12-29 14:47:21 +0545

    @types inside src

---
+++ src/@types/express/index.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+// declare namespace Express {
+// 	export interface Request {
+// 		data?: {
+// 			username: string;
+// 		}
+// 	}
+// }
