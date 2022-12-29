4df7620682ce2d6764697da00a934aab015a3ba0
AmritPaudel <46588187+amrit073@users.noreply.github.com>
2022-12-29 15:08:56 +0545

    fix tsconfig for including types from @types by ts-node

--- src/@types/express/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-// declare namespace Express {
-// 	export interface Request {
-// 		data?: {
-// 			username: string;
-// 		}
-// 	}
-// }
